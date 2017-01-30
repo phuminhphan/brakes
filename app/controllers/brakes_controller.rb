94f6bc96e8dacf10784d1f5594627c4ea71baf05
Tammy N <tammyngng@gmail.com>
2017-01-29 17:31:47 -0800

    added button to get brake data and store into database, then display the data as a table on index.html

--- app/controllers/brakes_controller.rb
+++ app/controllers/brakes_controller.rb
@@ -4,15 +4,19 @@ require 'nokogiri'
 require 'json/ext' # to use the C based extension instead of json/pure
 
 class BrakesController < ApplicationController
+
   def index
     @products = Product.all
   end
 
+  # GET gather_brake_data()
   def gather_brake_data
     years = (2016..2016).to_a #1 year only for now
     uri = URI.parse('https://www.r1concepts.com/home/allMake') #Get the makes
+
     http_client = Net::HTTP.new(uri.host, uri.port)
     http_client.use_ssl = true
+
     request = Net::HTTP::Post.new(uri.path) # For some reason, getting the makes require a HTTP POST
     request['X-Requested-With'] = 'XMLHttpRequest' #Specifies that this is AJAX
 
@@ -25,12 +29,13 @@ class BrakesController < ApplicationController
       page = Nokogiri::HTML(response.body)  # Read up on Nokogiri, it basically takes a string and turns into a virtual DOM
       makes = page.css('li').collect {|make_li| make_li.text} # With nokogiri, we can get the HTTP Response, turn it into a DOM, and run CSS3 selectors!
 
-      initialize_hash_keys(complete_data[year], makes)
+      initialize_hash_keys(complete_data[year], makes) #makes becomes the keys in the hash of complete_data
       makes.each do |make|
         make_get_uri = URI.parse("https://www.r1concepts.com/home/allModel?make=#{make}&year=#{year}")
         make_get_request = Net::HTTP::Get.new(make_get_uri) # Now for some reason, getting models is a HTTP GET LOL
         response = http_client.request(make_get_request)
         page = Nokogiri::HTML(response.body)
+
         models = page.css('li').collect {|model_li| model_li.text}
 
         initialize_hash_keys(complete_data[year][make], models)
@@ -42,9 +47,9 @@ class BrakesController < ApplicationController
           response = http_client.request(submodel_get_request)
           page = Nokogiri::HTML(response.body)
           submodels = page.css('li').collect {|submodel_li| submodel_li.text}
-
+          submodels.delete("All")
           initialize_hash_keys(complete_data[year][make][model], submodels)
-
+          
           # NOW MAKE THIS CALL: #https://www.r1concepts.com/listing/search/2016/Cadillac/SRX/4.6L_4627CC_V8_GAS_DOHC_Naturally_Aspirated
           # This call should get the products and go on from there.  Please take a look at db/migrate/create_products migration to see the attribute to fill in
         end
@@ -53,15 +58,26 @@ class BrakesController < ApplicationController
 
     end
 
-    ### Please insert complete_data into database
-    #Something like This
+    puts "@@@@@@@@@@@@@@@@complete_data now", JSON.pretty_generate(complete_data)
+
+    #Inserting into the Products table
     complete_data.each do |year, year_hash|
       year_hash.each do |make, make_hash|
+        make_hash.each do |model, model_hash|
+          model_hash.each do |submodel|
+            Product.find_or_create_by({year: year, make: make, model: model, submodel: submodel[0], product_name: "n/a"})
+            # for now add row to database but later we check if db has row already? if it does, don't do http request to server at all
+
+            
+
         #etc... You may need to double check the looping through of Hashes
         # All the way in, please insert into Products table
         # Product.create({year: year, make: make, model: model, submodel: submodel})
         # notice the other attributes aren't filled in... It's ok.  We can query those later but AS LONG AS WE HAVE THE PERMUTATIONS IN THE database
         # WE CAN literally make another method called "fetch_product_types_and_prices" to further update the record with description, product_name, price, category, position (rear or front) etc...
+            
+          end
+        end
       end
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root :to => 'brakes#index'
   resources :brakes
 
+  get '/gather_brake_data' => 'brakes#gather_brake_data', :as => 'brake_data'
+  put '/print_me' => 'brakes#print_me', :as => 'print_me'
 end
